0273970067524c0061800e451f125ab9220d844f
Josanu Andrei <andrei@quadra.md>
2024-06-26 10:29:52 +0300

    Add useLocation hook and create pathMatchRoute() func for route detecting and class adding for that menu item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,15 @@
+import {useLocation} from "react-router-dom";
+
 export default function Header() {
+    const location = useLocation();
+    console.log(location);
+
+    function pathMatchRoute(route) {
+        if(route === location.pathname) {
+            return true
+        }
+    }
+
     return (
         <div className="bg-white border-b shadow-sm sticky top-0 z-50">
             <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
@@ -11,7 +22,9 @@ export default function Header() {
                 </div>
                 <div>
                     <ul className="flex space-x-10">
-                        <li>Home</li>
+                        <li className={`py-3 text-sm font-semibold 
+                            text-gray-400 border-b-[3px] border-b-transparent 
+                            ${pathMatchRoute("/") && "text-black border-b-red-50"}`}>Home</li>
                         <li>Offers</li>
                         <li>Sign in</li>
                     </ul>
