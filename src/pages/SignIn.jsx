78fa0f65b6cfd73a5e1565a9d1b3dc583f013ef8
Josanu Andrei <andrei@quadra.md>
2024-06-26 17:41:45 +0300

    Start to create SignIn components with form and markup

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,48 @@
+import {useState} from "react";
+
 export default function SignIn() {
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const { email, password } = formData;
+
+    function onChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }));
+    }
+
+    function onSubmit(){
+        console.log('Form submitted');
+    }
+
     return (
-        <div>Sign In</div>
+        <section>
+            <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+            <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+                <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+                    <img
+                        src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
+                        alt="key"
+                        className="w-full rounded-2xl"
+                    />
+                </div>
+                <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={onChange}
+                            placeholder="Email address"
+                            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                        />
+                    </form>
+                </div>
+            </div>
+        </section>
     )
 }
