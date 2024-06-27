8a7069e546ec872048a1f952edfc575f7fc904a2
Josanu Andrei <andrei@quadra.md>
2024-06-27 16:40:52 +0300

    Finish with ForgotPassword page and did some fixes in SignUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -26,7 +26,7 @@ export default function SignUp() {
 
     return (
         <section>
-            <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+            <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
             <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
                 <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
                     <img
