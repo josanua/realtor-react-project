b9d133d209771f317768ac7be79de92ecf3d8bfb
Andrew <thrueword@gmail.com>
2025-07-11 10:47:47 +0300

    Last updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     const navigate = useNavigate();
     const auth = getAuth();
 
-    console.log(location);
+    console.log('Location: ' + location.pathname);
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
